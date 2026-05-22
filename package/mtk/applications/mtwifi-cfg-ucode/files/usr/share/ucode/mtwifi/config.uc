quytttb/immortalwrt-mt798x-rebase

commit 9baea4577a4d5c65d147e9e71bb64df01ab3a2db

mtk: mtwifi-cfg-ucode: drop async setup task for restoring vifs
an implicit race condition was found in post setup with async task, that netifd misses adding vifs (e.g. rax0) to bridge when doing setup process of second radio of DBDC card. Current logic is actually not robust enough to handle such situation:

- DBDC main vif not inited -> netifd caller script `set_up()` and waits
- DBDC main vif inited -> netifd caller script exit immediately

This wrongly assumes that we don't have to wait for vif present when netifd is notified through `set_up()`.  For detailed analysis, see:
https://github.com/chasey-dev/immortalwrt-mt798x-rebase/issues/22#issuecomment-4107512015

To fix this issue, we drop async task for restoring vifs. Though netifd watchdog timeout is 30s by default, single setup session doesn't reach this limit (up to about ~20s in DBDC main card reload), since `/sbin/wifi up` was introduced to restore sibling vifs in driver reload cases.

Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/ucode/mtwifi/config.uc
@@ -17,8 +17,6 @@
  */
 'use strict';
 
-import { task } from 'uloop';
-
 import * as datconf from 'datconf';
 import { defs } from 'mtwifi.defaults';
 import { log } from 'mtwifi.utils';
@@ -203,67 +201,58 @@ export function setup(uci_cfg, all_devs) {
 	let is_inited = driver.is_vif_inited(main_vif);
 
 	// post setup may need ~20s
-	// netifd kills main process if it waits too long, create a async task to do that
-	const postTask = task(() => {
-		// for DBDC cards, you need to init main dev first
-		if (is_dbdc && !is_inited) {
-			// just init main vif !
-			driver.init_dbdc_card(main_vif);
+	// for DBDC cards, you need to init main dev first
+	if (is_dbdc && !is_inited) {
+		// just init main vif !
+		driver.init_dbdc_card(main_vif);
+	}
+
+	// set vifs in current cfg UP first
+	// implict trace in raw netifd parameter:
+	// DISABLED vifs are not contained in uci_cfg.interfaces
+	// uci_cfg.interfaces will be EMPTY when uci_cfg.disabled = true, that current dev is disabled
+	// current trace:
+	// we read UCI cfg and added disabled ifaces from caller script
+	for (let idx, iface in uci_cfg.interfaces) {
+		let vif = iface.mtwifi_ifname;
+		let vif_cfg = iface.config;
+		log.debug(`[UCI] idx:${idx}, vif: ${vif}, disabled: ${vif_cfg.disabled ? true : false}, iface: ${iface}, iface cfg:${vif_cfg}`);
+
+		if (vif && !vif_cfg.disabled) {
+			driver.ifup(vif);
+			driver.apply_runtime_hooks(vif_cfg, vif);
 		}
+	}
+	
+	// for non DBDC cards, all set and return!
+	if (!is_dbdc) return;
 
-		// set vifs in current cfg UP first
-		// implict trace in raw netifd parameter:
-		// DISABLED vifs are not contained in uci_cfg.interfaces
-		// uci_cfg.interfaces will be EMPTY when uci_cfg.disabled = true, that current dev is disabled
-		// current trace:
-		// we read UCI cfg and added disabled ifaces from caller script
-		for (let idx, iface in uci_cfg.interfaces) {
-			let vif = iface.mtwifi_ifname;
-			let vif_cfg = iface.config;
-			log.debug(`[UCI] idx:${idx}, vif: ${vif}, disabled: ${vif_cfg.disabled ? true : false}, iface: ${iface}, iface cfg:${vif_cfg}`);
-
-			if (vif && !vif_cfg.disabled) {
-				driver.ifup(vif);
-				driver.apply_runtime_hooks(vif_cfg, vif);
+	// for DBDC cards, restore sibling devs
+	if (diff_res.need_reload) {
+		// in driver reload situation, 
+		// let another process to handle sib devs setup
+		for (let idx, devname in down_devnames) {
+			if (!(devname == cur_devname)) {
+				// use `&` symbol to run in the background,
+				// netifd may handle duplicated `wifi up [dev]` calls
+				// current lock context will not pass to it
+				system(`/sbin/wifi up ${devname} &`);
 			}
 		}
-		
-		// for non DBDC cards, all set and return!
-		if (!is_dbdc) return;
+	} else {
+		// in normal situation,
+		// for DBDC cards, restore vifs of sibling devs (if they were added before)
+		// just restore them in no need for reload situation
+		for (let vif in restore_vifs) {
+			log.notice(`[Main] Restoring sibling vif: ${vif}`);
+			driver.ifup(vif);
 
-		// for DBDC cards, restore sibling devs
-		if (diff_res.need_reload) {
-			// in driver reload situation, 
-			// let another process to handle sib devs setup
-			for (let idx, devname in down_devnames) {
-				if (!(devname == cur_devname)) {
-					// use `&` symbol to run in the background,
-					// netifd may handle duplicated `wifi up [dev]` calls
-					// current lock context will not pass to it
-					system(`/sbin/wifi up ${devname} &`);
-				}
-			}
-		} else {
-			// in normal situation,
-			// for DBDC cards, restore vifs of sibling devs (if they were added before)
-			// just restore them in no need for reload situation
-			for (let vif in restore_vifs) {
-				log.notice(`[Main] Restoring sibling vif: ${vif}`);
-				driver.ifup(vif);
-
-				// for apcli vifs, do apcli triggers
-				if (index(vif, "apcli") >= 0) {
-					driver.trigger_apcli(vif);
-				}
+			// for apcli vifs, do apcli triggers
+			if (index(vif, "apcli") >= 0) {
+				driver.trigger_apcli(vif);
 			}
 		}
-	});
-
-	// after task is created, just return and notify netifd
-	log.info(`[Main] Recovering vifs, create a task to do so. Task PID: ${postTask.pid()}`);
-
-	// let caller script know that main vif is inited or not
-	return is_inited;
+	}
 };
 
 export function down(cur_devname, all_devs) {

package/mtk/applications/mtwifi-cfg-ucode/Makefile
@@ -17,7 +17,6 @@ UCODE_DEPENDS:= \
   +ucode-mod-log \
   +ucode-mod-ubus \
   +ucode-mod-uci \
-  +ucode-mod-uloop \
   +ucode-mod-datconf \
   +ucode-mod-l1parser
 

package/mtk/applications/mtwifi-cfg-ucode/files/lib/netifd/wireless/mtwifi.sh
@@ -247,17 +247,11 @@ function handle_setup(data) {
 
     /*****          SETUP VIFS        *******/
     // UCI => DAT, ifup, reload driver...
-	let is_inited = cfg.setup(data, all_devs);
+    cfg.setup(data, all_devs);
     // notify netifd to setup
 	netifd.set_up();
 
 	l1.close();
-
-    // init main vifs may take ~10s, the task will hold the lock until its done
-    // if latter setup sessions come up so early, netifd may kill them if it waits too long!!!
-    if (!is_inited) {
-        sleep(10000);
-    }
 }
 
 // ==========================================

commit 2fa72c6b754dab94c451aefd5d761317734ce229

mtk: mtwifi-cfg-ucode: remove misleading comment Fixes: 9baea45
Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

package/mtk/applications/mtwifi-cfg-ucode/files/usr/share/ucode/mtwifi/config.uc
@@ -188,8 +188,6 @@ export function setup(uci_cfg, all_devs) {
 	}
 	
 	// no matter if it is DBDC card, we assume ifup main_vif is time-costy
-	// we dont want latter netifd setup sessions come up so early
-	// so we check here and decide whether we should sleep after creating the async task
 	let main_vif = "";
 	if (is_dbdc) {
 		// concat main dev name: ChipName_ChipIndex_1