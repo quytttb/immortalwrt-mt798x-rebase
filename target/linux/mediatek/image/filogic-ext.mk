c520c6955fdd5692563e6bad2b66c3b16b97fdbf
speeed01 <894846254@qq.com>
2026-05-25 18:36:08 +0800

    mediatek: filogic: add MTK U-Boot layout support for Xiaomi Redmi Router AX6000
    This variant is NOT compatible with stock or ubootmod.
    Link: https://github.com/chasey-dev/immortalwrt-mt798x-rebase/pull/45

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -150,3 +150,20 @@ ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
 endif
 endef
 TARGET_DEVICES += wirelesstag_zx7981pd-ubootmod
+
+define Device/xiaomi_redmi-router-ax6000-mtkuboot
+  DEVICE_VENDOR := Xiaomi
+  DEVICE_MODEL := Redmi Router AX6000
+  DEVICE_VARIANT := (MTK U-Boot layout)
+  DEVICE_DTS := mt7986a-xiaomi-redmi-router-ax6000-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 112640k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += xiaomi_redmi-router-ax6000-mtkuboot
