68331a91966323ea2b0071bb7d38f6b43b7a2a3f
Hai Quy <quyquaquyet02tx@gmail.com>
2026-06-27 23:02:51 +0700

    fix: replace luci-app-argon-config with luci-app-aurora-config in device packages

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -144,7 +144,7 @@ define Device/viettel_nr3053
   IMAGE/sysupgrade.itb := append-kernel | \
 	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | \
 	append-metadata
-  DEVICE_PACKAGES := default-settings-vn luci-app-argon-config bndstrg \
+  DEVICE_PACKAGES := default-settings-vn luci-app-aurora-config bndstrg \
 	-kmod-usb3 -kmod-usb-ledtrig-usbport -automount -autosamba
   ARTIFACTS := preloader.bin bl31-uboot.fip
   ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
@@ -171,7 +171,7 @@ define Device/vht_32x6
   IMAGE/sysupgrade.itb := append-kernel | \
 	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | \
 	append-metadata
-  DEVICE_PACKAGES := default-settings-vn luci-app-argon-config bndstrg \
+  DEVICE_PACKAGES := default-settings-vn luci-app-aurora-config bndstrg \
 	-kmod-usb3 -kmod-usb-ledtrig-usbport -automount -autosamba
   ARTIFACTS := preloader.bin bl31-uboot.fip
   ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
